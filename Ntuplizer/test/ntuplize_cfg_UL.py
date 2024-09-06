0cb1ec650d186f82315af67b4ba93a38da9aa961
Justin Marquez <justin.marquez@wisc.edu>
2024-09-06 09:33:43 -0500

    added load statements as listed for EgammaPostReco (may be optional)

--- Ntuplizer/test/ntuplize_cfg_UL.py
+++ Ntuplizer/test/ntuplize_cfg_UL.py
@@ -213,7 +213,9 @@ if options.profile:
 
 # Basic stuff for all jobs
 process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
-
+process.load("Configuration.StandardSequences.Services_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Geometry.CaloEventSetup.CaloTowerConstituents_cfi")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 from Configuration.AlCa.GlobalTag import GlobalTag
 
