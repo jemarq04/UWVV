39d013c87fe671ccae570f47fc72f8b157d8fc2c
Justin Marquez <justin.marquez@wisc.edu>
2024-10-04 10:49:42 -0500

    modified to be more versatile with extra options for cmsRun

--- Ntuplizer/test/runMC.sh
+++ Ntuplizer/test/runMC.sh
@@ -1,23 +1,19 @@
 if [[ $# -lt 2 ]]; then
-	echo "usage: $0 FILE YEAR [LHE]"
+	echo "usage: $0 FILE YEAR [EXTRA...]"
   echo 
   echo "FILE: input file listing input miniAOD ROOT files, 1 per line."
   echo "YEAR: any year in Run 3 for analysis"
-  echo "LHE: optional argument to provide lheWeights variable to config file. (default: 0)"
+  echo "EXTRA: optional argument(s) passed directly to cmsRun"
 	exit 1
 fi
 
-if [[ ! -f "${1}" ]]; then
-	echo "input file ${1} not found"
+infile=$1
+year=$2
+shift 2
+
+if [[ ! -f $infile ]]; then
+	echo "input file $infile not found"
 	exit 2
 fi
 
-lhe=$3
-if [[ -z $lhe ]]; then
-  lhe=0
-elif [[ ! $lhe =~ ^[0-9]+$ ]]; then
-  echo "invalid lheWeights value"
-  exit 3
-fi
-
-cmsRun ntuplize_cfg.py inputFileList=$1 year=$2 lheWeights=$lhe channels=zz isMC=1 eCalib=1 muCalib=1 genInfo=1
+cmsRun ntuplize_cfg.py inputFileList=$infile year=$year channels=zz isMC=1 eCalib=1 muCalib=1 genInfo=1 $@
