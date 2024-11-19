f8853fd6dddb9e554e12ae998a977e3a9bd15c17
Justin Marquez <justin.marquez@wisc.edu>
2024-11-19 10:15:10 -0600

    added script to partner 'runMC.sh' for running data jobs to test

---
+++ Ntuplizer/test/runData.sh
@@ -0,0 +1,19 @@
+if [[ $# -lt 2 ]]; then
+	echo "usage: $0 FILE YEAR [EXTRA...]"
+  echo 
+  echo "FILE: input file listing input miniAOD ROOT files, 1 per line."
+  echo "YEAR: any year in Run 3 for analysis"
+  echo "EXTRA: optional argument(s) passed directly to cmsRun"
+	exit 1
+fi
+
+infile=$1
+year=$2
+shift 2
+
+if [[ ! -f $infile ]]; then
+	echo "input file $infile not found"
+	exit 2
+fi
+
+cmsRun ntuplize_cfg.py inputFileList=$infile year=$year channels=zz isMC=0 eCalib=1 muCalib=1 genInfo=0 $@
