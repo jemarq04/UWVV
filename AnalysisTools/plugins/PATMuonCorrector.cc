246731aa6b645f68588f674defb7d0f060f7d673
Justin Marquez <justin.marquez@wisc.edu>
2025-03-12 13:29:59 -0500

    suppress uncertainty variations for pt corrections as they do not currently work

--- AnalysisTools/plugins/PATMuonCorrector.cc
+++ AnalysisTools/plugins/PATMuonCorrector.cc
@@ -84,14 +84,18 @@ void PATMuonCorrector::produce(edm::Event& iEvent, const edm::EventSetup& iSetup
     
     double uncorr_pt = mi->pt();
     double corr_pt   = getCorrectedPt(mptr);
-    double syst_pt   = getCorrectedPt(mptr, "syst");
-    double stat_pt   = getCorrectedPt(mptr, "stat");
 
-    out->back().setP4(reco::Particle::PolarLorentzVector(corr_pt, mi->eta(), mi->phi(), mi->mass()));
     out->back().addUserFloat("uncorrected_pt", uncorr_pt);
     out->back().addUserFloat("ptScaleFactor", corr_pt/uncorr_pt);
-    out->back().addUserFloat("syst_pt", syst_pt);
-    out->back().addUserFloat("stat_pt", stat_pt);
+    /* TODO: Updated muon corrections removed 'syst' and 'stat' variations on k_data, which is 
+     *  necessary for calculating any of these variations for MC at the moment. Once this is fixed,
+     *  these can be added back in.
+    if (isMC_){
+      out->back().addUserFloat("syst_pt", getCorrectedPt(mptr, "syst"));
+      out->back().addUserFloat("stat_pt", getCorrectedPt(mptr, "stat"));
+    }
+     */
+    out->back().setP4(reco::Particle::PolarLorentzVector(corr_pt, mi->eta(), mi->phi(), mi->mass()));
   }
 
   iEvent.put(std::move(out));
