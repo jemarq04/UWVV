2def33faaab1c17c99fa8062d225a41d3e30012a
Justin Marquez <justin.marquez@wisc.edu>
2025-03-17 14:23:00 -0500

    change printouts. add optional 'printInfo' for more verbose information

--- AnalysisTools/plugins/PATObjectCounter.cc
+++ AnalysisTools/plugins/PATObjectCounter.cc
@@ -42,17 +42,21 @@ public:
 private:
   virtual void produce(edm::Event& iEvent, const edm::EventSetup& iSetup);
 
+  void printInfo(edm::Ptr<T> obj, int index=0);
+
+  const edm::InputTag srcTag_;
   const edm::EDGetTokenT<edm::View<T> > srcToken_;
   const std::vector<std::string> cut_strings_;
   const std::vector<std::string> labels_;
-  const bool verbose_;
+  const bool verbose_, doPrintInfo_;
   std::vector<StringCutObjectSelector<T>> cuts_;
 };
 
 
 template<typename T>
 PATObjectCounter<T>::PATObjectCounter(const edm::ParameterSet& iConfig) :
-  srcToken_(consumes<edm::View<T> >(iConfig.getParameter<edm::InputTag>("src"))),
+  srcTag_(iConfig.getParameter<edm::InputTag>("src")),
+  srcToken_(consumes<edm::View<T> >(srcTag_)),
   cut_strings_(iConfig.exists("cuts") ?
              iConfig.getParameter<std::vector<std::string> >("cuts") :
              std::vector<std::string>()),
@@ -61,6 +65,9 @@ PATObjectCounter<T>::PATObjectCounter(const edm::ParameterSet& iConfig) :
              std::vector<std::string>()),
   verbose_(iConfig.exists("verbose") ?
              iConfig.getParameter<bool>("verbose") :
+             false),
+  doPrintInfo_(iConfig.exists("printInfo") ?
+             iConfig.getParameter<bool>("printInfo") :
              false)
 {
   if(cut_strings_.size() != labels_.size())
@@ -81,28 +88,40 @@ PATObjectCounter<T>::PATObjectCounter(const edm::ParameterSet& iConfig) :
 
 
 template<typename T>
-void PATObjectCounter<T>::produce(edm::Event& iEvent,
-                                         const edm::EventSetup& iSetup)
+void PATObjectCounter<T>::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
   edm::Handle<edm::View<T> > in;
   iEvent.getByToken(srcToken_, in);
-  
+
   for (size_t i = 0; i < cut_strings_.size(); i++) 
+  {
+    std::unique_ptr<int> num(new int(0));
+    if (cut_strings_[i] != "")
     {
-      std::unique_ptr<int> num(new int(0));
-      if (cut_strings_[i] != "")
-        {
-          for(size_t j = 0; j < in->size(); j++)
-            {
-              if (cuts_[i](in->at(j)))
-                  (*num)++;
-            }
-        }
-      else
-          *num = in->size();
-      if (verbose_) std::cout << "Count: " << labels_[i] << " = " << *num << std::endl;
-      iEvent.put(std::move(num), labels_[i]);
+      for(size_t j = 0; j < in->size(); j++)
+        if (cuts_[i](in->at(j))) (*num)++;
     }
+    else
+      *num = in->size();
+
+    if (verbose_) std::cout << srcTag_.label() << " Count: " << labels_[i] << " = " << *num << std::endl;
+    iEvent.put(std::move(num), labels_[i]);
+  }
+  if (doPrintInfo_){
+    for (size_t j=0; j<in->size(); j++){
+      edm::Ptr<T> obj(in, j);
+      printInfo(obj, j);
+    }
+  }
+}
+
+template <typename T>
+void PATObjectCounter<T>::printInfo(edm::Ptr<T> obj, int index){}
+template <>
+void PATObjectCounter<pat::Muon>::printInfo(edm::Ptr<pat::Muon> muon, int index){
+  std::cout << "  Object " << index << std::endl;
+  std::cout << "    pT = " << muon->pt() << std::endl;
+  std::cout << "    type = " << muon->type() << std::endl;
 }
 
 typedef PATObjectCounter<pat::Electron> PATElectronCounter;
