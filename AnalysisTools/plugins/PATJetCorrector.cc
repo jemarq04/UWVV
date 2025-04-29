c65dcc1febc1d3c439a269600d364593e045dae6
Justin Marquez <justin.marquez@wisc.edu>
2025-04-29 09:52:20 -0500

    remove jet smearing so that this file only considers jet energy scaling (JES) and its uncertainties

--- AnalysisTools/plugins/PATJetCorrector.cc
+++ AnalysisTools/plugins/PATJetCorrector.cc
@@ -44,9 +44,9 @@ private:
 
   edm::EDGetTokenT<JetView> srcToken;
   edm::EDGetTokenT<double> rhoToken;
-  std::string scaleFileName_, smearFileName_, jesConfig_, jerConfig_;
+  std::string scaleFileName_, smearFileName_, config_;
   std::unique_ptr<correction::CorrectionSet> scaleFile_, smearFile_;
-  const bool isMC_;
+  const bool isMC_, systematics_;
   edm::ConsumesCollector cc;
   edm::ESGetToken<JetCorrectorParametersCollection,JetCorrectionsRecord> jecToken;
 };
@@ -56,12 +56,9 @@ PATJetCorrector::PATJetCorrector(const edm::ParameterSet& iConfig) :
   srcToken(consumes<JetView>(iConfig.getParameter<edm::InputTag>("src"))),
   rhoToken(consumes<double>(iConfig.getParameter<edm::InputTag>("rhoSrc"))),
   scaleFileName_(iConfig.getParameter<std::string>("scaleFile")),
-  smearFileName_(iConfig.exists("smearFile") ?
-      iConfig.getParameter<std::string>("smearFile") : 
-      "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/JME/jer_smear.json.gz"),
-  jesConfig_(iConfig.getParameter<std::string>("jesConfig")),
-  jerConfig_(iConfig.getParameter<std::string>("jerConfig")),
+  config_(iConfig.getParameter<std::string>("config")),
   isMC_(iConfig.exists("isMC") ? iConfig.getParameter<bool>("isMC") : false),
+  systematics_(iConfig.exists("systematics") ? iConfig.getParameter<bool>("systematics") : isMC_),
   cc(consumesCollector()),
   jecToken(cc.esConsumes(edm::ESInputTag("","AK4PFPuppi")))
 {
@@ -81,9 +78,7 @@ PATJetCorrector::PATJetCorrector(const edm::ParameterSet& iConfig) :
   }
 
   produces<VJet>();
-  if (isMC_){
-    produces<VJet>("jerUp");
-    produces<VJet>("jerDown");
+  if (systematics_){
     produces<VJet>("jesUp");
     produces<VJet>("jesDown");
   }
@@ -105,81 +100,38 @@ void PATJetCorrector::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   std::unique_ptr<VJet> out(new VJet());
   std::unique_ptr<VJet> out_jesUp(new VJet());
   std::unique_ptr<VJet> out_jesDn(new VJet());
-  std::unique_ptr<VJet> out_jerUp(new VJet());
-  std::unique_ptr<VJet> out_jerDn(new VJet());
 
-  std::string jesName = jesConfig_ + (isMC_? "_MC" : "_DATA") + "_L1L2L3Res_AK4PFPuppi";
-  std::string jerName = jerConfig_ + "_MC_PtResolution_AK4PFPuppi";
-  std::string jersfName = jerConfig_ + "_MC_ScaleFactor_AK4PFPuppi";
+  std::string jesName = config_ + (isMC_? "_MC" : "_DATA") + "_L1L2L3Res_AK4PFPuppi";
 
-  for (size_t i = 0; i<in->size(); ++i){
+  for (size_t i = 0; i<in->size(); ++i)
+  {
     const Jet& jet = in->at(i);
-    out->push_back(jet);
-    if (isMC_){
-      out_jesUp->push_back(jet);
-      out_jesDn->push_back(jet);
-      out_jerUp->push_back(jet);
-      out_jerDn->push_back(jet);
-    }
 
     // JES
     double jes = scaleFile_->compound().at(jesName)->evaluate({jet.jetArea(), jet.eta(), jet.pt(), *rho});
+    out->push_back(jet);
     out->back().setP4(math::XYZTLorentzVector(jes * jet.p4()));
 
-    if (isMC_){
-      // Get jet values
-      float eta = out->back().eta();
-      float pt = out->back().pt();
-      const reco::GenJet* gen = jet.genJet();
-      float genpt = (gen!=nullptr)? gen->pt() : -1.0;
+    if (systematics_){
+      const Jet& jetCorr = out->back();
 
       // JES Uncertainty
-      jecUnc.setJetEta(jet.eta());
-      jecUnc.setJetPt(jet.pt());
+      jecUnc.setJetEta(jetCorr.eta());
+      jecUnc.setJetPt(jetCorr.pt());
       float unc = jecUnc.getUncertainty(true);
 
-      out_jesUp->back().setP4(math::XYZTLorentzVector((1.+unc) * out_jesUp->back().p4()));
-      out_jesDn->back().setP4(math::XYZTLorentzVector((1.-unc) * out_jesDn->back().p4()));
+      out_jesUp->push_back(jetCorr);
+      out_jesUp->back().setP4(math::XYZTLorentzVector((1.+unc) * jetCorr.p4()));
 
-      // JER
-      double jer       = scaleFile_->at(jerName)->evaluate({eta, pt, *rho});
-      double jersf     = scaleFile_->at(jersfName)->evaluate({eta, pt, "nom"});
-      double jerCorr   = smearFile_->at("JERSmear")->evaluate({pt, eta, genpt, *rho, int(iEvent.id().event()), jer, jersf});
-      out->back().setP4(math::XYZTLorentzVector(jerCorr * jet.p4()));
-      out->back().addUserFloat("jerCorrInverse", 1./jerCorr);
-
-      // JER Uncertainty
-      double jerUp     = scaleFile_->at(jerName)->evaluate({eta, pt, *rho});
-      double jerDn     = scaleFile_->at(jerName)->evaluate({eta, pt, *rho});
-      double jersfUp   = scaleFile_->at(jersfName)->evaluate({eta, pt, "up"});
-      double jersfDn   = scaleFile_->at(jersfName)->evaluate({eta, pt, "down"});
-      double jerCorrUp = smearFile_->at("JERSmear")->evaluate({pt, eta, genpt, *rho, int(iEvent.id().event()), jerUp, jersfUp});
-      double jerCorrDn = smearFile_->at("JERSmear")->evaluate({pt, eta, genpt, *rho, int(iEvent.id().event()), jerDn, jersfDn});
-      out_jerUp->back().setP4(math::XYZTLorentzVector(jerCorrUp * jet.p4()));
-      out_jerUp->back().addUserFloat("jerCorrInverse", 1./jerCorrUp);
-      out_jerDn->back().setP4(math::XYZTLorentzVector(jerCorrDn * jet.p4()));
-      out_jerDn->back().addUserFloat("jerCorrInverse", 1./jerCorrDn);
-
-      // JER Smearing on JES Uncertainties
-      double jer_jesUp     = scaleFile_->at(jerName)->evaluate({eta, out_jesUp->back().pt(), *rho});
-      double jer_jesDn     = scaleFile_->at(jerName)->evaluate({eta, out_jesDn->back().pt(), *rho});
-      double jersf_jesUp   = scaleFile_->at(jersfName)->evaluate({eta, out_jesUp->back().pt(), "nom"});
-      double jersf_jesDn   = scaleFile_->at(jersfName)->evaluate({eta, out_jesDn->back().pt(), "nom"});
-      double jerCorr_jesUp = smearFile_->at("JERSmear")->evaluate({out_jesUp->back().pt(), eta, genpt, *rho, int(iEvent.id().event()), jer_jesUp, jersf_jesUp});
-      double jerCorr_jesDn = smearFile_->at("JERSmear")->evaluate({out_jesDn->back().pt(), eta, genpt, *rho, int(iEvent.id().event()), jer_jesDn, jersf_jesDn});
-      out_jesUp->back().setP4(math::XYZTLorentzVector(jerCorr_jesUp * out_jesUp->back().p4()));
-      out_jesUp->back().addUserFloat("jerCorrInverse", 1./jerCorr_jesUp);
-      out_jesDn->back().setP4(math::XYZTLorentzVector(jerCorr_jesDn * out_jesDn->back().p4()));
-      out_jesDn->back().addUserFloat("jerCorrInverse", 1./jerCorr_jesDn);
+      out_jesDn->push_back(jetCorr);
+      out_jesDn->back().setP4(math::XYZTLorentzVector((1.-unc) * jetCorr.p4()));
     }
   }
 
   iEvent.put(std::move(out));
-  if (isMC_){
+  if (systematics_){
     iEvent.put(std::move(out_jesUp), "jesUp");
     iEvent.put(std::move(out_jesDn), "jesDown");
-    iEvent.put(std::move(out_jerUp), "jerUp");
-    iEvent.put(std::move(out_jerDn), "jerDown");
   }
 }
 
