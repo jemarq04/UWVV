29f47b9c160ea797e73fcd6e895af4b744ccc1ed
Justin Marquez <justin.marquez@wisc.edu>
2025-04-29 15:07:11 -0500

    remove passPUID function (old) - instead access calculated fullId. re-indent and clean up code. update tight jet ID for UL analysis

--- AnalysisTools/plugins/PATJetIDEmbedder.cc
+++ AnalysisTools/plugins/PATJetIDEmbedder.cc
@@ -29,83 +29,78 @@ typedef edm::View<Jet> JetView;
 
 class PATJetIDEmbedder : public edm::stream::EDProducer<>
 {
- public:
-  explicit PATJetIDEmbedder(const edm::ParameterSet& pset);
-  virtual ~PATJetIDEmbedder() {;}
+  public:
+    explicit PATJetIDEmbedder(const edm::ParameterSet& pset);
+    virtual ~PATJetIDEmbedder() {;}
 
- private:
-  virtual void produce(edm::Event& iEvent, const edm::EventSetup& iSetup);
+  private:
+    virtual void produce(edm::Event& iEvent, const edm::EventSetup& iSetup);
 
-  bool passTight(const Jet& jet) const;
-  bool passPUID(const Jet& jet) const;
-  typedef edm::Association<reco::GenJetCollection> MatchMap;
-  edm::EDGetTokenT<JetView> srcToken;
-  edm::EDGetTokenT<MatchMap> matchToken_;
-  bool domatch_;
-  const int  setup_;
-  int PUid;
-  int evtcount = 0;
-  int jetcount = 0;
-  
+    bool passTight(const Jet& jet) const;
+    typedef edm::Association<reco::GenJetCollection> MatchMap;
+    edm::EDGetTokenT<JetView> srcToken;
+    edm::EDGetTokenT<MatchMap> matchToken_;
+    bool domatch_;
+    const int setup_;
+    int evtcount = 0;
+    int jetcount = 0;
+
 };
 
 
 PATJetIDEmbedder::PATJetIDEmbedder(const edm::ParameterSet& pset) :
   srcToken(consumes<JetView>(pset.getParameter<edm::InputTag>("src"))),
   matchToken_(consumes<MatchMap>(edm::InputTag("patJetGenJetMatch"))),
-  domatch_(pset.exists("domatch") ?
-	   pset.getParameter<bool>("domatch") :
-	   false),
+  domatch_(pset.exists("domatch") ? pset.getParameter<bool>("domatch") : false),
   //Which year JET ID we need
-  setup_(pset.exists("setup") ?
-	   pset.getParameter<int>("setup") :
-	   2016)
+  setup_(pset.exists("setup") ? pset.getParameter<int>("setup") : 2016)
 {
   produces<VJet>();
 }
 
 
-void PATJetIDEmbedder::produce(edm::Event& iEvent,
-                               const edm::EventSetup& iSetup)
+void PATJetIDEmbedder::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-  //evtcount++;
   //printf("====================RECO vs Gen jet Information=========================================\n");
-  //printf("evt#   pt     eta    phi    pt     eta    phi    PUid0 PUidnew jet#\n");
+  //printf("evt#   pt     eta    phi    pt     eta    phi    idPU0 idPUnew jet#\n");
+  //evtcount++;
   //jetcount = 0;
   edm::Handle<JetView> in;
   iEvent.getByToken(srcToken, in);
   edm::Handle<MatchMap> match;
-  if (domatch_){
-  iEvent.getByToken(matchToken_, match);}
+  if (domatch_)
+    iEvent.getByToken(matchToken_, match);
 
   std::unique_ptr<VJet> out(new VJet());
 
-  for(size_t i = 0; i < in->size(); ++i)
-    {
-      out->push_back(in->at(i)); // copies, transfers ownership
+  for (size_t i = 0; i < in->size(); ++i)
+  {
+    out->push_back(in->at(i)); // copies, transfers ownership
+    Jet& jet = out->back();
 
-      Jet& jet = out->back();
+    int idPU = jet.userInt("pileupJetIdUpdated:fullId");
+    jet.addUserFloat("idTight", float(passTight(jet)));
+    jet.addUserFloat("idPU", float(idPU));
 
-      jet.addUserFloat("idTight", float(passTight(jet)));
-      jet.addUserFloat("idPU", float(passPUID(jet)));
+    if (domatch_){
+      edm::Ref<JetView> jetRef(in, i);
+      const auto genMatched = (*match)[jetRef];
+      jet.addUserFloat("genjetMatched", genMatched.isNonnull()? 1. : 0.);
 
-      if (domatch_){
-        //jetcount++;
-        edm::Ref<JetView> jetRef(in, i);
-        const auto genMatched = (*match)[jetRef];
-        PUid = jetRef->userInt("pileupJetIdUpdated:fullId");
-        if (genMatched.isNonnull()){
-          //printf("%3d %7.2f %6.2f %6.2f %7.2f %6.2f %6.2f %5d %5d %7d\n", 
-         //evtcount, jetRef->pt(), jetRef->eta(), jetRef->phi(), genMatched->pt(), genMatched->eta(), genMatched->phi(),jetRef->userInt("pileupJetId:fullId"), PUid, jetcount);
-          jet.addUserFloat("genjetMatched", 1.);
-        }
-        else{
-          //printf("%3d %7.2f %6.2f %6.2f %7.2f %6.2f %6.2f %5d %5d %7d\n", 
-         //evtcount, jetRef->pt(), jetRef->eta(), jetRef->phi(), -1.,-1.,-1.,jetRef->userInt("pileupJetId:fullId"), PUid, jetcount);
-          jet.addUserFloat("genjetMatched", 0.);
-        }
+      /*
+      jetcount++;
+      if (genMatched.isNonnull()){
+        printf("%3d %7.2f %6.2f %6.2f %7.2f %6.2f %6.2f %5d %5d %7d\n", 
+          evtcount, jet.pt(), jet.eta(), jet.phi(), genMatched->pt(), genMatched->eta(), genMatched->phi(),jet.userInt("pileupJetId:fullId"), idPU, jetcount);
       }
+      else{
+        printf("%3d %7.2f %6.2f %6.2f %7.2f %6.2f %6.2f %5d %5d %7d\n", 
+          evtcount, jet.pt(), jet.eta(), jet.phi(), -1.,-1.,-1.,jet.userInt("pileupJetId:fullId"), idPU, jetcount);
+        jet.addUserFloat("genjetMatched", 0.);
+      }
+      */
     }
+  }
 
   iEvent.put(std::move(out));
 }
@@ -125,61 +120,25 @@ bool PATJetIDEmbedder::passTight(const Jet& jet) const
   bool JetID = false;
 
   if ( setup_ == 2016 )
-  {// Tight jet ID https://twiki.cern.ch/twiki/bin/view/CMS/JetID13TeVRun2016
-    JetID = ((NHF<0.90 && NEMF<0.90 && NumConst>1) && ((absEta<=2.4 && CHF>0 && CHM>0 && CEMF<0.99) || absEta>2.4) && absEta<=2.7) ||
-            ( NHF<0.98 && NEMF>0.01 && NumNeutralParticles>2 && absEta>2.7 && absEta<=3.0 ) ||
-            ( NEMF<0.90 && NumNeutralParticles>10 && absEta >3.0 );
-    //JetID = ((NHF<0.99 && NEMF<0.99 && NumConst>1) && ((absEta<=2.4 && CHF>0 && CHM>0 && CEMF<0.99) || absEta>2.4) && absEta<=2.7) ||
-    //        ( NHF<0.98 && NEMF>0.01 && NumNeutralParticles>2 && absEta>2.7 && absEta<=3.0 ) ||
-    //        ( NEMF<0.90 && NumNeutralParticles>10 && absEta >3.0 );
-  
-}
-  else if ( setup_ == 2017 )
-  {// Tight jet ID https://twiki.cern.ch/twiki/bin/view/CMS/JetID13TeVRun2017 without JetIDLepVeto
-   JetID  = ((NHF<0.90 && NEMF<0.90 && NumConst>1) && ((absEta<=2.4 && CHF>0 && CHM>0) || absEta>2.4) && absEta<=2.7) ||
-            ( NEMF<0.99 && NEMF>0.02 && NumNeutralParticles>2 && absEta>2.7 && absEta<=3.0 ) ||
-            ( NEMF<0.90 && NHF>0.02 && NumNeutralParticles>10 && absEta>3.0 );
-  }
-  else if ( setup_ == 2018) 
-  {// Tight jet ID https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetID13TeVRun2018 without JetIDLepVeto
-    JetID = ( CHM>0 && CHF>0 && NumConst>1 && NEMF<0.9 && NHF < 0.9 && absEta<=2.6) ||
-            ( CHM>0 && NEMF<0.99 && NHF < 0.9 && absEta>2.6 && absEta<=2.7) ||
-            ( NEMF>0.02 && NEMF<0.99 && NumNeutralParticles>2 && absEta>2.7 && absEta<=3.0 ) ||
-            ( NEMF<0.90 && NHF>0.2 && NumNeutralParticles>10 && absEta>3.0 );
-  }
-  else
   {
-    throw cms::Exception("JetID") << "Jet ID is not defined for the given setup (" << setup_ << ")!";
+    // Tight jet ID https://twiki.cern.ch/twiki/bin/view/CMS/JetID13TeVUL#Recommendations_for_the_13_TeV_U
+    JetID = (absEta <= 2.4 && NHF < 0.90 && NEMF < 0.90 && NumConst > 1 && CHF > 0 && CEMF > 0) ||
+            (absEta > 2.4 && absEta <= 2.7 && NHF < 0.90 && NEMF < 0.99) ||
+            (absEta > 2.7 && absEta <= 3.0 && NHF < 0.90 && NEMF > 0 && NEMF < 0.99 && NumNuetralParticles > 1) ||
+            (absEta > 3.0 && NHF > 0.2 && NEMF < 0.90 && NumNeutralParticles>10);
   }
+  else if ( setup_ == 2017 || setup_ == 2018)
+  {
+    // Tight jet ID https://twiki.cern.ch/twiki/bin/view/CMS/JetID13TeVUL#Recommendations_for_the_13_T_AN1
+    JetID = (absEta <= 2.4 && NHF < 0.90 && NEMF < 0.90 && NumConst > 1 && CHF > 0 && CEMF > 0) ||
+            (absEta > 2.4 && absEta <= 2.7 && NHF < 0.90 && NEMF < 0.99 && CHM > 0) ||
+            (absEta > 2.7 && absEta <= 3.0 && NEMF > 0.01 && NEMF < 0.99 && NumNeutralParticles > 1) ||
+            (absEta > 3.0 && NHF > 0.2 && NEMF < 0.9 && NumNeutralParticles > 10);
+  }
+  else throw cms::Exception("JetID") << "Jet ID is not defined for the given setup (" << setup_ << ")!";
+
   return JetID;
 }
-bool PATJetIDEmbedder::passPUID(const Jet& jet) const
-{
-  if(!jet.hasUserFloat("pileupJetId:fullDiscriminant"))
-    return false;
-
-  float mva = jet.userFloat("pileupJetId:fullDiscriminant");
-
-  float absEta = std::abs(jet.eta());
-
-  if(jet.pt() > 20.)
-    {
-      if(absEta > 3. && mva <= -0.45) return false;
-      if(absEta > 2.75 && mva <= 0.55) return false;
-      if(absEta > 2.5 && mva <= -0.6) return false;
-      if(mva <= -0.63) return false;
-    }
-  else
-    {
-      if(absEta > 3. && mva <= -0.95) return false;
-      if(absEta > 2.75 && mva <= -0.94) return false;
-      if(absEta > 2.5 && mva <= -0.96) return false;
-      if(mva <= -0.95) return false;
-    }
-
-  return true;
-}
-
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(PATJetIDEmbedder);
