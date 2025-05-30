43461ffbd5cb9a085d6fbc7e58f6e7af99661bdc
Justin Marquez <justin.marquez@wisc.edu>
2025-05-30 08:05:03 -0500

    reinstate electron cut for FSR photons to reflect HZZ workflow

--- AnalysisTools/python/templates/ZZIso.py
+++ AnalysisTools/python/templates/ZZIso.py
@@ -18,7 +18,7 @@ class ZZIso(AnalysisFlowBase):
                 isoDecisionLabel = cms.string(self.getZZIsoLabel()),
                 isoValueLabel = cms.string(self.getZZIsoLabel().replace("Pass", "Val")),
                 fsrLabel = cms.string(self.getFSRLabel()),
-                #fsrElecSelection = cms.string('userFloat("%s") > 0.5'%self.getZZIDLabel()),
+                fsrElecSelection = cms.string('userFloat("%s") > 0.5'%self.getZZIDLabel()),
                 fsrMuonSelection = cms.string('userFloat("%s") > 0.5'%self.getZZIDLabel()),
                 eaLabel = cms.string('EffectiveArea'),
                 isoConeDRMaxE = cms.double(0.3),
@@ -27,16 +27,10 @@ class ZZIso(AnalysisFlowBase):
                 #value to a large number because the UserFloat ZZIsoPass is being used in other modules such as ZZCategoryEmbedder in "leptonSelector".
                 #Need to figure out a smarter way other than having a separate leptonSelector for electrons and muons
                 isoCutE = cms.double(99999),
-                #Isolation and SIP cuts are no longer needed since these variables are included in the new Muon BDT
-                #But I'm keeping this cut for now in case its needed for HighPtMuons(pt>200) with HighPtTrkID though its unlikely
                 isoCutMu = cms.double(0.35),
                 )
             step.addModule('leptonIsoEmbedding', leptonIsoEmbedding,
                            'e', 'm', e='electrons', m='muons')
-            #The isolation cut is applied after recovered FSR photons are subtracted 
-            #from the isolation cone (see below) only for muons since electron isolation variables are now included in the MVA!.
-            #step.addModule('leptonIsoEmbedding', leptonIsoEmbedding,
-            #               'm', m='muons')
 
         return step
 
