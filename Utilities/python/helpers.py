ceb0e8a070a3632a9520ef2e5ca0d18b693f1d7e
Justin Marquez <justin.marquez@wisc.edu>
2025-06-22 19:12:52 -0500

    add noqa for ruff checks

--- Utilities/python/helpers.py
+++ Utilities/python/helpers.py
@@ -133,7 +133,7 @@ def mapObjects(channel):
     Return a list of objects of the form ['e1','e2','m1','m2'] or ['e1','e2','m']
     Objects are in alphabetical/numerical order order
     '''
-    global _zzhelpers_object_maps_
+    global _zzhelpers_object_maps_ #noqa: PLW0602
 
     try:
         return _zzhelpers_object_maps_[channel]
