289367919d3cf93ddd313dd40030a956a5cdda98
Justin Marquez <justin.marquez@wisc.edu>
2025-09-03 12:55:07 -0500

    remove break statements in loops

--- Ntuplizer/test/runTest.sh
+++ Ntuplizer/test/runTest.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
 for year in 2022 2023 2024; do
-  echo $year
   for f in inputs/testing/${year}*.dat; do
+    [[ ! -f $f ]] && break
+    echo $f
     [[ $f = *Data* ]] && isMC=0 || isMC=1
     extra=""
     [[ $year = 2022 && $isMC ]] && extra="dataPeriod=C"
     cmsRun ntuplize_cfg.py inputFileList=$f year=$year channels=zz isMC=$isMC eCalib=1 muCalib=1 genInfo=$isMC maxEvents=100 outputFile=testing.root $extra
     #cmsRun ntuplize_cfg.py inputFileList=$f year=$year channels=zl isMC=$isMC eCalib=1 muCalib=1 genInfo=$isMC maxEvents=100 outputFile=testing.root $extra
-    break
   done
-  break
 done
 
 rm -f testing*.root
