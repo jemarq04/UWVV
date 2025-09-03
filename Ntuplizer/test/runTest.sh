369e0665703a26473aa05466fb348907f5e4e947
Justin Marquez <justin.marquez@wisc.edu>
2025-09-03 13:17:41 -0500

    fix postEE and postBPix treatment in test

--- Ntuplizer/test/runTest.sh
+++ Ntuplizer/test/runTest.sh
@@ -6,7 +6,9 @@ for year in 2022 2023 2024; do
     echo $f
     [[ $f = *Data* ]] && isMC=0 || isMC=1
     extra=""
-    [[ $year = 2022 && $isMC ]] && extra="dataPeriod=C"
+    [[ $year = 2022 && $isMC ]] && extra="$extra dataPeriod=C"
+    [[ $year = 2022 && $f = *postEE* ]] && extra="$extra postEE=1"
+    [[ $year = 2023 && $f = *postBPix* ]] && extra="$extra postBPix=1"
     cmsRun ntuplize_cfg.py inputFileList=$f year=$year channels=zz isMC=$isMC eCalib=1 muCalib=1 genInfo=$isMC maxEvents=100 outputFile=testing.root $extra
     #cmsRun ntuplize_cfg.py inputFileList=$f year=$year channels=zl isMC=$isMC eCalib=1 muCalib=1 genInfo=$isMC maxEvents=100 outputFile=testing.root $extra
   done
