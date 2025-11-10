4de8b62c9f6a90a52d71a8ae20b0666fbd5495d1
Justin Marquez <justin.marquez@wisc.edu>
2025-11-10 16:05:14 -0600

    Revert "increase default maximum memory per job"
    This reverts commit f648b81ad4afb5726b4a1ebe5fd79d5c67b08973.

--- Utilities/scripts/crab.py
+++ Utilities/scripts/crab.py
@@ -223,7 +223,6 @@ config.JobType.allowUndistributedCMSSW = True
 config.JobType.psetName = '%s/src/UWVV/Ntuplizer/test/ntuplize_cfg.py' % os.environ["CMSSW_BASE"]
 config.JobType.numCores = 1
 config.JobType.inputFiles = ["%s/src/UWVV/data" % os.environ["CMSSW_BASE"]]
-config.JobType.maxMemoryMB = int(localSettings["maxMemoryMB"]) #(default is 2000)
 
 config.Data.inputDBS = 'global' if 'USER' not in dataset else 'phys03'
 #config.Data.allowNonValidInputDataset = True
