d349a5618cfb4637bd78930136918b073178253a
Justin Marquez <justin.marquez@wisc.edu>
2026-01-09 17:56:39 -0600

    Revert "apply CMSSW formatting"
    This reverts commit ef4ccf4745ce0680f031e2529efc94d611b29be4.

--- Utilities/interface/helpers.h
+++ Utilities/interface/helpers.h
@@ -1,6 +1,7 @@
 #ifndef UWVV_Utilities_helpers_h
 #define UWVV_Utilities_helpers_h
 
+
 #include <string>
 
 #include "TLorentzVector.h"
@@ -12,30 +13,35 @@
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "DataFormats/PatCandidates/interface/Jet.h"
 
-namespace uwvv {
+namespace uwvv
+{
 
-  namespace helpers {
+  namespace helpers
+  {
     // Get an object's four-momentum with FSR included (if any).
     // Note: p4WithoutFSR<pat::CompositeCandidate> is defined in helpers.cc
-    template <class T>
-    math::XYZTLorentzVector p4WithoutFSR(const T& cand) {
+    template<class T>
+    math::XYZTLorentzVector p4WithoutFSR(const T& cand)
+    {
       // if it's not a composite, FSR isn't included by definition
-      if (cand.numberOfDaughters() == 0)
+      if(cand.numberOfDaughters() == 0)
         return cand.p4();
 
       const pat::CompositeCandidate& ccand = dynamic_cast<const pat::CompositeCandidate&>(cand);
       //return p4WithoutFSR(ccand);
       math::XYZTLorentzVector out = p4WithoutFSR(*(ccand.daughter(0)->masterClone().get()));
-      if (ccand.numberOfDaughters() >= 2)
+      if(ccand.numberOfDaughters() >= 2)
         out += p4WithoutFSR(*(ccand.daughter(1)->masterClone().get()));
 
       return out;
     }
 
-    template <class T>
-    math::XYZTLorentzVector p4WithoutFSR(const edm::Ptr<T>& cand) {
+    template<class T>
+    math::XYZTLorentzVector p4WithoutFSR(const edm::Ptr<T>& cand)
+    {
       return p4WithoutFSR(*cand);
     }
+
 
     float zMassDistance(const float m);
 
@@ -50,15 +56,17 @@ namespace uwvv {
     // Check if any final daughters of mother are within dR of cand.
     // "final daughters" means it checks the daughters of daughters if
     // applicable
-    bool overlapWithAnyDaughter(const reco::Candidate& cand, const reco::Candidate& mother, float dR);
+    bool overlapWithAnyDaughter(const reco::Candidate& cand,
+                                const reco::Candidate& mother, float dR);
 
     // Return the jet collection cleaned from the initial state objects.
     // Collection should be embedded into the initial state as userData.
     const edm::PtrVector<pat::Jet>* getCleanedJetCollection(const pat::CompositeCandidate& cand,
-                                                            const std::string& variation,
-                                                            std::string collectionName = "cleanedJets");
-  }  // namespace helpers
+        const std::string& variation, std::string collectionName="cleanedJets");
+  } // namespace helpers
 
-}  // namespace uwvv
+} // namespace uwvv
 
-#endif  // header guard
+
+
+#endif // header guard
