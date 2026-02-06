3b038a8098ed48c9f470b7faa4e7183e40dc471e
Justin Marquez <justin.marquez@wisc.edu>
2026-02-06 07:29:45 -0600

    fix: remove redundant yearstring assignment for 2025

--- AnalysisTools/python/templates/ElectronCalibration.py
+++ AnalysisTools/python/templates/ElectronCalibration.py
@@ -62,8 +62,6 @@ class ElectronCalibration(AnalysisFlowBase):
                 yearstring += "EE"
             elif self.year == "2023" and self.calibEra23 == "postBPix":
                 yearstring += "BPix"
-            elif self.year == "2025":
-                yearstring = "2025"
             scaleFile = getCorrectionFile("EGM", yearstring, "electronSS_EtDependent.json.gz")
 
             # Electron corrections
