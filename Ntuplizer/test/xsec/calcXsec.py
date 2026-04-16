46606c0f87f882cd3f0c13c083e12125ab913413
Justin Marquez <justin.marquez@wisc.edu>
2026-04-16 08:04:10 -0500

    feat: add NanoAOD xsec calculation

---
+++ Ntuplizer/test/xsec/calcXsec.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python3
+
+import argparse
+from array import array
+
+import ROOT
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
+
+
+class GenZZXsecAnalyzer(Module):
+    def __init__(self, scale=1.0, useStatusFlags=False, total_units_in_pb=True):
+        self.writeHistFile = True
+        self.scale = scale
+        self.useStatusFlags = useStatusFlags
+        self.total_units_in_pb = total_units_in_pb
+
+        self.numEventsTotal = 0
+        self.sumWeightsTotal = 0.0
+        self.sumWeightsOnShell = [0.0, 0.0, 0.0]
+        self.sumWeightsFiducial = [0.0, 0.0, 0.0]
+
+    def selectionOnShell(self, zCands):
+        #return 40 < zCands[0].mass < 120 and 4 < zCands[1].mass < 120
+        return all(60 < part.mass < 120 for part in zCands)
+
+    def selectionFiducial(self, leptons):
+        if any(lep.eta > 2.5 or lep.pt < 5 for lep in leptons):
+            return False
+
+        for i in range(len(leptons)):
+            for j in range(i + 1, len(leptons)):
+                if leptons[i].pdgId == -leptons[j].pdgId and (leptons[i].p4() + leptons[j].p4()).M() < 4:
+                    return False
+
+        leppt = sorted([lep.pt for lep in leptons])
+        if leppt[0] < 20 or leppt[1] < 10:
+            return False
+
+        return True
+
+    def beginJob(self, histFile=None, histDirName=None):
+        Module.beginJob(self, histFile, histDirName)
+
+        self.addObject(ROOT.TH1F("h_numZs", "Number of Z Bosons", 10, 0, 10))
+
+        self.addObject(ROOT.TH1F("h_genWeight", "Generator Weight", 100, 0, 0))
+        self.addObject(
+            ROOT.TH1F(
+                "h_ZMass",
+                "Z Candidate Mass",
+                16,
+                array("d", [0, 2, 4, 7, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]),
+            )
+        )
+        self.addObject(
+            ROOT.TH1F(
+                "h_ZMass_2e2m",
+                "Z Candidate Mass",
+                16,
+                array("d", [0, 2, 4, 7, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]),
+            )
+        )
+        self.addObject(
+            ROOT.TH1F(
+                "h_ZMass_4l",
+                "Z Candidate Mass",
+                16,
+                array("d", [0, 2, 4, 7, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]),
+            )
+        )
+
+    def endJob(self):
+        Module.endJob(self)
+
+        separator = "---------"
+        channels = ["eemm", "mmmm", "eeee"]
+
+        def printXsec(label, sumWeights, unit="pb", scale=1.0):
+            scale *= 1.0 if unit == "pb" else 1000.0
+            sumWeights *= scale
+            #denom = self.numEventsTotal
+            denom = self.sumWeightsTotal
+
+            print(f"{label:14} xsec: {sumWeights/denom:.3f} {unit} ({sumWeights:.3f}/{denom:.3f})")
+
+        print(separator)
+        unit = "pb" if self.total_units_in_pb else "fb"
+        printXsec("Total", self.sumWeightsTotal, unit, self.scale)
+
+        print(separator)
+        for i, chan in enumerate(channels):
+            printXsec(f"On Shell {chan}", self.sumWeightsOnShell[i], "fb", self.scale) 
+
+        print(separator)
+        for i, chan in enumerate(channels):
+            printXsec(f"Fiducial {chan}", self.sumWeightsFiducial[i], "fb", self.scale) 
+
+        print(separator)
+        print()
+
+    def analyze(self, event):
+        weight = event.genWeight
+        #weight = event.Generator_weight
+
+        self.numEventsTotal += 1
+        self.sumWeightsTotal += weight
+        self.h_genWeight.Fill(weight)
+
+        # Define collections
+        genparticles = Collection(event, "GenPart")
+        if self.useStatusFlags:
+            leptons = [
+                part
+                for part in genparticles
+                if abs(part.pdgId) in [11, 13] and part.status == 1 and part.statusFlags & (1 << 8)
+            ]
+        else:
+            leptons = [
+                part
+                for part in genparticles
+                if abs(part.pdgId) in [11, 13]
+                and part.genPartIdxMother >= 0
+                and abs(genparticles[part.genPartIdxMother].pdgId) == 23
+            ]
+
+        if len(leptons) < 4:
+            return False
+        elif len(leptons) > 4:
+            print(f"WARNING: Over 4 final state leptons!")
+            return False
+
+        leptons = sorted(leptons, key=lambda part: part.genPartIdxMother)
+        zCands = [genparticles[idx] for idx in list(set([lep.genPartIdxMother for lep in leptons]))]
+        #if len(zCands) != 2: print("WARNING: More than two Z bosons:", len(zCands))
+        # zCands = [genparticles[leptons[i].genPartIdxMother] for i in [0, 2]]
+        num_electrons = sum(1 for lep in leptons if abs(lep.pdgId) == 11)
+        num_electrons_to_channel = {
+            2: 0,
+            0: 1,
+            4: 2,
+        }
+        channel = num_electrons_to_channel[num_electrons]
+
+        # Fill Z mass histograms
+        for cand in zCands:
+            self.h_ZMass.Fill(cand.mass)
+            if num_electrons == 2:
+                self.h_ZMass_2e2m.Fill(cand.mass)
+            else:
+                self.h_ZMass_4l.Fill(cand.mass)
+
+        # Apply on-shell cut
+        if not self.selectionOnShell(zCands):
+            return False
+        self.sumWeightsOnShell[channel] += weight
+
+        # Apply fiducial cut
+        if not self.selectionFiducial(leptons):
+            return False
+        self.sumWeightsFiducial[channel] += weight
+
+        return True
+
+
+def main():
+    ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+    sample_map = {
+        "qqZZ_run2": {
+            "files": [
+                "root://cmsxrootd.fnal.gov//store/mc/RunIISummer16NanoAODv3/ZZTo4L_13TeV_powheg_pythia8/NANOAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3-v1/100000/26F9E9B6-33C7-E811-86C7-001A649D4A45.root"
+            ],
+            "scale": 1.256,
+        },
+        "ggZZ4e_run2": {
+            "files": [
+                "root://cmsxrootd.fnal.gov//store/mc/RunIISummer16NanoAODv3/GluGluToContinToZZTo4e_13TeV_MCFM701_pythia8/NANOAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3-v2/20000/FE68BB95-03FA-E811-A4A1-509A4C72D5CE.root",
+            ],
+            "scale": 0.00159,
+        },
+        "ggZZ2e2m_run2": {
+            "files": [
+                "root://cmsxrootd.fnal.gov//store/mc/RunIISummer16NanoAODv3/GluGluToContinToZZTo2e2mu_13TeV_MCFM701_pythia8/NANOAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3-v2/110000/B40666DF-A1EB-E811-B01B-001517FB21CC.root",
+            ],
+            "scale": 0.00319,
+        },
+        "qqZZ_run3": {
+            "files": [
+                # "root://cmsxrootd.fnal.gov//store/mc/Run3Summer22NanoAODv12/ZZto4L_TuneCP5_13p6TeV_powheg-pythia8/NANOAODSIM/130X_mcRun3_2022_realistic_v5-v2/2520000/56a9348d-c4ae-4f88-8ba3-502e7dfca8ad.root",
+                "root://cmsxrootd.fnal.gov//store/mc/Run3Summer22NanoAODv12/ZZto4L_TuneCP5_13p6TeV_powheg-pythia8/NANOAODSIM/130X_mcRun3_2022_realistic_v5-v2/2520000/1f701563-9fb0-4ef5-be31-9cd38c73dbf1.root"
+            ],
+            "scale": 1.390,
+        },
+    }
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--sample", choices=sample_map.keys(), required=True, help="sample to calculate")
+    parser.add_argument("--maxEvents", type=int, default=10_000, help="maximum number of entries")
+    parser.add_argument("--statusFlags", action="store_true", help="identify final state leptons with status flags")
+    args = parser.parse_args()
+
+    if args.maxEvents < 0:
+        args.maxEvents = None
+
+    mod_args = {
+        "useStatusFlags": args.statusFlags,
+    }
+
+    p = PostProcessor(
+        ".",
+        sample_map[args.sample]["files"],
+        cut=None,
+        branchsel=None,
+        maxEntries=args.maxEvents,
+        modules=[
+            GenZZXsecAnalyzer(
+                scale=sample_map[args.sample].get("scale", 1.0), total_units_in_pb="ggZZ" not in args.sample, **mod_args
+            )
+        ],
+        noOut=True,
+        histFileName=f"histout_{args.sample}.root",
+        histDirName=args.sample,
+    )
+    p.run()
+
+
+if __name__ == "__main__":
+    main()
