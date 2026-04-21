324b09b176fa0666a43dc57d1b4375199e26d780
Justin Marquez <justin.marquez@wisc.edu>
2026-04-21 15:23:42 -0500

    feat: update runXsec helper scripts

--- Ntuplizer/test/runXsec.sh
+++ Ntuplizer/test/runXsec.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-dasgoclient -limit 50 -query "file dataset=/ZZto4L_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM" > .temp_inputs.dat
-cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat 2>&1 | tee out_zz4l.txt
+maxFiles=50
+maxEvents=100000
 
-dasgoclient -limit 50 -query "file dataset=/GluGlutoContinto2Zto4E_TuneCP5_13p6TeV_mcfm-pythia8/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM" > .temp_inputs.dat
-cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat scale=0.00305851 2>&1 | tee out_ggZZ4e.txt
+dasgoclient -limit $maxFiles -query "file dataset=/ZZto4L_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM" > .temp_inputs.dat
+cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat maxEvents=$maxEvents 2>&1 | tee out_zz4l.txt
 
-dasgoclient -limit 50 -query "file dataset=/GluGlutoContinto2Zto4Mu_TuneCP5_13p6TeV_mcfm-pythia8/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM" > .temp_inputs.dat
-cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat scale=0.00305851 2>&1 | tee out_ggZZ4m.txt
+dasgoclient -limit $maxFiles -query "file dataset=/GluGlutoContinto2Zto4E_TuneCP5_13p6TeV_mcfm-pythia8/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM" > .temp_inputs.dat
+cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat maxEvents=$maxEvents scale=0.00305851 2>&1 | tee out_ggZZ4e.txt
 
-dasgoclient -limit 50 -query "file dataset=/GluGlutoContinto2Zto2E2Mu_TuneCP5_13p6TeV_mcfm701-pythia8/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v1/MINIAODSIM" > .temp_inputs.dat
-cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat scale=0.00624157 2>&1 | tee out_ggZZ2e2m.txt
+dasgoclient -limit $maxFiles -query "file dataset=/GluGlutoContinto2Zto4Mu_TuneCP5_13p6TeV_mcfm-pythia8/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM" > .temp_inputs.dat
+cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat maxEvents=$maxEvents scale=0.00305851 2>&1 | tee out_ggZZ4m.txt
+
+dasgoclient -limit $maxFiles -query "file dataset=/GluGlutoContinto2Zto2E2Mu_TuneCP5_13p6TeV_mcfm701-pythia8/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v1/MINIAODSIM" > .temp_inputs.dat
+cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat maxEvents=$maxEvents scale=0.00624157 2>&1 | tee out_ggZZ2e2m.txt
 
 rm .temp_inputs.dat
--- Ntuplizer/test/runXsec_run2.sh
+++ Ntuplizer/test/runXsec_run2.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-dasgoclient -limit 50 -query "file dataset=/ZZTo4L_13TeV_powheg_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM" > .temp_inputs.dat
-cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat scale=1.256 maxEvents=-1 2>&1 | tee out_zz4l_run2.txt
+maxFiles=50
+maxEvents=100000
 
-dasgoclient -limit 50 -query "file dataset=/GluGluToContinToZZTo4e_13TeV_MCFM701_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM" > .temp_inputs.dat
-cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat scale=0.00270241 maxEvents=-1 2>&1 | tee out_ggZZ4e_run2.txt
+dasgoclient -limit $maxFiles -query "file dataset=/ZZTo4L_13TeV_powheg_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM" > .temp_inputs.dat
+cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat scale=1.256 maxEvents=$maxEvents 2>&1 | tee out_zz4l_run2.txt
 
-#dasgoclient -limit 5 -query "file dataset=/GluGluToContinToZZTo4mu_13TeV_MCFM701_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM" > .temp_inputs.dat
-#cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat scale=0.00270241 maxEvents=-1 2>&1 | tee out_ggZZ4m_run2.txt
+dasgoclient -limit $maxFiles -query "file dataset=/GluGluToContinToZZTo4e_13TeV_MCFM701_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM" > .temp_inputs.dat
+#dasgoclient -limit $maxFiles -query "file dataset=/GluGluToContinToZZTo4e_13TeV_MCFM701_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/MINIAODSIM" > .temp_inputs.dat
+cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat scale=0.00159 maxEvents=$maxEvents 2>&1 | tee out_ggZZ4e_run2.txt
 
-#dasgoclient -limit 5 -query "file dataset=/GluGluToContinToZZTo2e2mu_13TeV_MCFM701_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM" > .temp_inputs.dat
-#cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat scale=0.00542294 maxEvents=-1 2>&1 | tee out_ggZZ2e2m_run2.txt
+dasgoclient -limit $maxFiles -query "file dataset=/GluGluToContinToZZTo4mu_13TeV_MCFM701_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM" > .temp_inputs.dat
+cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat scale=0.00270241 maxEvents=$maxEvents 2>&1 | tee out_ggZZ4m_run2.txt
+
+dasgoclient -limit $maxFiles -query "file dataset=/GluGluToContinToZZTo2e2mu_13TeV_MCFM701_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM" > .temp_inputs.dat
+#dasgoclient -limit $maxFiles -query "file dataset=/GluGluToContinToZZTo2e2mu_13TeV_MCFM701_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v3/MINIAODSIM" > .temp_inputs.dat
+cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat scale=0.00319 maxEvents=$maxEvents 2>&1 | tee out_ggZZ2e2m_run2.txt
 
 rm .temp_inputs.dat
