afccb9274b3da9c037d83ee3c3c372b358d5103a
Justin Marquez <justin.marquez@wisc.edu>
2026-04-21 18:25:07 -0500

    fix: add xsec scale for qqZZ

--- Ntuplizer/test/runXsec.sh
+++ Ntuplizer/test/runXsec.sh
@@ -4,7 +4,7 @@ maxFiles=50
 maxEvents=100000
 
 dasgoclient -limit $maxFiles -query "file dataset=/ZZto4L_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM" > .temp_inputs.dat
-cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat maxEvents=$maxEvents 2>&1 | tee out_zz4l.txt
+cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat maxEvents=$maxEvents scale=1.390 2>&1 | tee out_zz4l.txt
 
 dasgoclient -limit $maxFiles -query "file dataset=/GluGlutoContinto2Zto4E_TuneCP5_13p6TeV_mcfm-pythia8/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM" > .temp_inputs.dat
 cmsRun xsec_cfg.py inputFileList=.temp_inputs.dat maxEvents=$maxEvents scale=0.00305851 2>&1 | tee out_ggZZ4e.txt
